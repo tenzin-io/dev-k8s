150622e2d62a2592ee2cac83490b803fdacaab0b
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-08-24 20:56:26 -0400

    Update nfs-subdir to use release version

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ module "nginx_ingress" {
 }
 
 module "nfs_subdir" {
-  source     = "git::https://github.com/tenzin-io/terraform-tenzin-nfs-subdir.git?ref=main"
+  source     = "git::https://github.com/tenzin-io/terraform-tenzin-nfs-subdir.git?ref=v0.0.4"
   nfs_server = "zfs-1.tenzin.io"
   nfs_path   = "/data/homelab-k8s-dev"
 }
