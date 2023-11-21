a87bfa4e3a64a4c1516e76eb5f1870bcf1392199
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-11-21 18:16:27 -0500

    Enable modules

--- main.tf
+++ main.tf
@@ -7,24 +7,24 @@ terraform {
   }
 }
 
-#module "cert_manager" {
-#  source                  = "git::https://github.com/tenzin-io/terraform-tenzin-cert-manager.git?ref=v0.0.2"
-#  cert_registration_email = "tenzin@tenzin.io"
-#  cloudflare_api_token    = data.vault_generic_secret.cloudflare.data.api_token
-#}
+module "cert_manager" {
+  source                  = "git::https://github.com/tenzin-io/terraform-tenzin-cert-manager.git?ref=v0.0.2"
+  cert_registration_email = "tenzin@tenzin.io"
+  cloudflare_api_token    = data.vault_generic_secret.cloudflare.data.api_token
+}
 
 module "metallb" {
   source        = "git::https://github.com/tenzin-io/terraform-tenzin-metallb.git?ref=v0.0.1"
   ip_pool_range = "192.168.200.240/32"
 }
 
-#module "nginx_ingress" {
-#  source                  = "git::https://github.com/tenzin-io/terraform-tenzin-nginx-ingress-controller.git?ref=v0.0.2"
-#  depends_on              = [module.metallb, module.cert_manager]
-#}
+module "nginx_ingress" {
+  source     = "git::https://github.com/tenzin-io/terraform-tenzin-nginx-ingress-controller.git?ref=v0.0.2"
+  depends_on = [module.metallb, module.cert_manager]
+}
 
-#module "nfs_subdir" {
-#  source     = "git::https://github.com/tenzin-io/terraform-tenzin-nfs-subdir.git?ref=v0.0.4"
-#  nfs_server = "zfs-1"
-#  nfs_path   = "/data/dev-k8s"
-#}
+module "nfs_subdir" {
+  source     = "git::https://github.com/tenzin-io/terraform-tenzin-nfs-subdir.git?ref=v0.0.4"
+  nfs_server = "zfs-1"
+  nfs_path   = "/data/dev-k8s"
+}
