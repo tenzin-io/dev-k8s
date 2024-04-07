b2cdcb59608424f0853b624a622fcfce45305a5b
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-04-07 11:18:01 -0400

    Add tailscale secret

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
-data "vault_generic_secret" "github_app" {
-  path = "secrets/github_app"
-}
-
 data "vault_generic_secret" "cloudflare" {
   path = "secrets/cloudflare"
+}
+
+data "vault_generic_secret" "tailscale" {
+  path = "secrets/tailscale/kubernetes_ingress"
 }
