9d04eb91005e10b2718e2484a430d211a29113c4
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-04-07 11:18:21 -0400

    Update input vars for environment

--- main.tf
+++ main.tf
@@ -15,16 +15,19 @@ module "cert_manager" {
 
 module "metallb" {
   source        = "git::https://github.com/tenzin-io/terraform-tenzin-metallb.git?ref=v0.0.1"
-  ip_pool_range = "192.168.200.240/32"
+  ip_pool_range = "192.168.7.12/32"
 }
 
 module "nginx_ingress" {
-  source     = "git::https://github.com/tenzin-io/terraform-tenzin-nginx-ingress-controller.git?ref=v0.0.2"
-  depends_on = [module.metallb, module.cert_manager]
+  source                  = "git::https://github.com/tenzin-io/terraform-tenzin-nginx-ingress-controller.git?ref=v0.0.3"
+  depends_on              = [module.metallb, module.cert_manager]
+  enable_tailscale_tunnel = true
+  tailscale_auth_key      = data.vault_generic_secret.tailscale.data.tailscale_auth_key
+
 }
 
 module "nfs_subdir" {
   source     = "git::https://github.com/tenzin-io/terraform-tenzin-nfs-subdir.git?ref=v0.0.4"
-  nfs_server = "zfs-1"
-  nfs_path   = "/data/dev-k8s"
+  nfs_server = "localhost"
+  nfs_path   = "/data"
 }
