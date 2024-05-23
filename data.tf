70d8a12ace2b34d54b9aad44f684e80d6b3303b7
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-05-23 03:11:55 +0000

    Comment out Vault use

--- data.tf
+++ data.tf
@@ -1,11 +1,12 @@
-data "vault_generic_secret" "cloudflare" {
-  path = "secrets/cloudflare"
-}
-
-data "vault_generic_secret" "tailscale" {
-  path = "secrets/tailscale/kubernetes_ingress"
-}
-
-data "vault_generic_secret" "jupyterhub" {
-  path = "secrets/jupyterhub"
-}
+#data "vault_generic_secret" "cloudflare" {
+#  path = "secrets/cloudflare"
+#}
+#
+#data "vault_generic_secret" "tailscale" {
+#  path = "secrets/tailscale/kubernetes_ingress"
+#}
+#
+#data "vault_generic_secret" "jupyterhub" {
+#  path = "secrets/jupyterhub"
+#}
+#
