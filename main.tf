1c3ba7c33693445e77c01139385ce939fbf36bc2
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-05-25 17:26:46 +0000

    Use new module repo

--- main.tf
+++ main.tf
@@ -17,37 +17,49 @@ module "local_path_provisioner" {
 }
 
 module "cert_manager" {
-  source     = "git::https://github.com/tenzin-io/terraform-tenzin-homelab.git//kubernetes/cert-manager?ref=main"
-  cloudflare_api_token    = var.cloudflare_api_token
+  source                     = "git::https://github.com/tenzin-io/terraform-tenzin-homelab.git//kubernetes/cert-manager?ref=main"
+  cloudflare_api_token       = var.cloudflare_api_token
   enable_lets_encrypt_issuer = true
+  depends_on                 = [module.calico]
 }
 
 module "metallb" {
-  source     = "git::https://github.com/tenzin-io/terraform-tenzin-homelab.git//kubernetes/metallb?ref=main"
+  source        = "git::https://github.com/tenzin-io/terraform-tenzin-homelab.git//kubernetes/metallb?ref=main"
   ip_pool_range = "192.168.200.230/32"
+  depends_on    = [module.calico]
 }
 
-#
-#module "nginx_ingress" {
-#  source                  = "git::https://github.com/tenzin-io/terraform-tenzin-nginx-ingress-controller.git?ref=v0.0.3"
-#  depends_on              = [module.metallb, module.cert_manager]
-#  enable_tailscale_tunnel = true
-#  tailscale_auth_key      = data.vault_generic_secret.tailscale.data.tailscale_auth_key
-#
-#}
-#
-#module "nfs_subdir" {
-#  source     = "git::https://github.com/tenzin-io/terraform-tenzin-nfs-subdir.git?ref=v0.0.4"
-#  nfs_server = "localhost"
-#  nfs_path   = "/data"
-#}
-#
-#module "jupyterhub" {
-#  source                       = "git::https://github.com/tenzin-io/terraform-tenzin-jupyterhub.git?ref=v0.0.1"
-#  jupyterhub_fqdn              = "jupyterhub.tenzin.io"
-#  cert_issuer_name             = module.cert_manager.cert_issuer_name
-#  github_oauth_client_id       = data.vault_generic_secret.jupyterhub.data.github_oauth_client_id
-#  github_oauth_client_secret   = data.vault_generic_secret.jupyterhub.data.github_oauth_client_secret
-#  allowed_github_organizations = ["tenzin-io", "tenzinlab"]
-#}
-#
+module "nginx_ingress" {
+  source                  = "git::https://github.com/tenzin-io/terraform-tenzin-homelab.git//kubernetes/ingress-nginx?ref=main"
+  enable_tailscale_tunnel = true
+  tailscale_auth_key      = var.tailscale_auth_key
+  tailscale_hostname      = "dev-k8s-ingress-nginx"
+  depends_on              = [module.metallb, module.cert_manager]
+}
+
+module "jupyterhub" {
+  depends_on                   = [module.cert_manager, module.nginx_ingress, module.local_path_provisioner]
+  source                       = "git::https://github.com/tenzin-io/terraform-tenzin-homelab.git//kubernetes/jupyterhub?ref=main"
+  jupyterhub_fqdn              = "jupyterhub.tenzin.io"
+  cert_issuer_name             = module.cert_manager.cert_issuer_name
+  enable_github_oauth          = true
+  github_oauth_client_id       = var.jupyterhub_github_oauth_client_id
+  github_oauth_client_secret   = var.jupyterhub_github_oauth_client_secret
+  allowed_github_organizations = ["tenzin-io"]
+}
+
+module "prometheus" {
+  depends_on = [module.cert_manager, module.local_path_provisioner]
+  source     = "git::https://github.com/tenzin-io/terraform-tenzin-homelab.git//kubernetes/prometheus?ref=main"
+}
+
+module "grafana" {
+  depends_on                 = [module.cert_manager, module.local_path_provisioner, module.prometheus]
+  source                     = "git::https://github.com/tenzin-io/terraform-tenzin-homelab.git//kubernetes/grafana?ref=main"
+  grafana_fqdn               = "grafana.tenzin.io"
+  cert_issuer_name           = module.cert_manager.cert_issuer_name
+  enable_github_oauth        = true
+  github_oauth_client_id     = var.grafana_github_oauth_client_id
+  github_oauth_client_secret = var.grafana_github_oauth_client_secret
+  allowed_github_organization = "tenzin-io"
+}
--- variables.tf
+++ variables.tf
@@ -1,4 +1,29 @@
 variable "cloudflare_api_token" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
+}
+
+variable "tailscale_auth_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "jupyterhub_github_oauth_client_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "jupyterhub_github_oauth_client_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "grafana_github_oauth_client_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "grafana_github_oauth_client_secret" {
+  type      = string
+  sensitive = true
 }
