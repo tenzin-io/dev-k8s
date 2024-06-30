6875e6a9ec1df8e3ec0f38ad4e55af33dbc4adba
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-06-30 22:56:04 +0000

    Use Vault

--- data.tf
+++ data.tf
@@ -1,12 +1,19 @@
-#data "vault_generic_secret" "cloudflare" {
-#  path = "secrets/cloudflare"
-#}
-#
-#data "vault_generic_secret" "tailscale" {
-#  path = "secrets/tailscale/kubernetes_ingress"
-#}
-#
-#data "vault_generic_secret" "jupyterhub" {
-#  path = "secrets/jupyterhub"
-#}
-#
+data "vault_generic_secret" "cloudflare" {
+  path = "secrets/cloudflare"
+}
+
+data "vault_generic_secret" "grafana" {
+  path = "secrets/grafana"
+}
+
+data "vault_generic_secret" "jupyterhub" {
+  path = "secrets/jupyterhub"
+}
+
+data "vault_generic_secret" "github_tenzin_org" {
+  path = "secrets/github-actions/tenzin-io"
+}
+
+data "vault_generic_secret" "github_tlhakhan_user" {
+  path = "secrets/github-actions/tlhakhan"
+}
--- main.tf
+++ main.tf
@@ -1,4 +1,24 @@
 terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 4.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
     bucket         = "tenzin-io"
     key            = "terraform/dev-k8s.state"
@@ -18,7 +38,7 @@ module "local_path_provisioner" {
 
 module "cert_manager" {
   source                     = "git::https://github.com/tenzin-io/terraform-tenzin-homelab.git//kubernetes/cert-manager?ref=main"
-  cloudflare_api_token       = var.cloudflare_api_token
+  cloudflare_api_token       = data.vault_generic_secret.cloudflare.data["cloudflare_api_token"]
   enable_lets_encrypt_issuer = true
   depends_on                 = [module.calico]
 }
@@ -32,7 +52,7 @@ module "metallb" {
 module "nginx_ingress" {
   source                   = "git::https://github.com/tenzin-io/terraform-tenzin-homelab.git//kubernetes/ingress-nginx?ref=main"
   enable_cloudflare_tunnel = true
-  cloudflare_tunnel_token  = var.cloudflare_tunnel_token
+  cloudflare_tunnel_token  = data.vault_generic_secret.cloudflare.data["cloudflare_tunnel_token"]
   depends_on               = [module.metallb, module.cert_manager]
 }
 
@@ -42,8 +62,8 @@ module "jupyterhub" {
   jupyterhub_fqdn              = "jupyterhub.tenzin.io"
   cert_issuer_name             = module.cert_manager.cert_issuer_name
   enable_github_oauth          = true
-  github_oauth_client_id       = var.jupyterhub_github_oauth_client_id
-  github_oauth_client_secret   = var.jupyterhub_github_oauth_client_secret
+  github_oauth_client_id       = data.vault_generic_secret.jupyterhub.data["github_oauth_client_id"]
+  github_oauth_client_secret   = data.vault_generic_secret.jupyterhub.data["github_oauth_client_secret"]
   allowed_github_organizations = ["tenzin-io"]
 }
 
@@ -58,17 +78,36 @@ module "grafana" {
   grafana_fqdn                = "grafana.tenzin.io"
   cert_issuer_name            = module.cert_manager.cert_issuer_name
   enable_github_oauth         = true
-  github_oauth_client_id      = var.grafana_github_oauth_client_id
-  github_oauth_client_secret  = var.grafana_github_oauth_client_secret
+  github_oauth_client_id      = data.vault_generic_secret.grafana.data["github_oauth_client_id"]
+  github_oauth_client_secret  = data.vault_generic_secret.grafana.data["github_oauth_client_secret"]
   allowed_github_organization = "tenzin-io"
 }
 
 module "actions_runner" {
-  source                     = "git::https://github.com/tenzin-io/terraform-tenzin-homelab.git//kubernetes/actions-runner?ref=main"
-  runner_set_name            = "dev-k8s"
-  runner_image               = "ghcr.io/tenzin-io/actions-runner:v0.0.1"
-  github_organization_url    = "https://github.com/tenzin-io"
-  github_app_id              = var.actions_runner_github_app_id
-  github_app_installation_id = var.actions_runner_github_app_installation_id
-  github_app_private_key     = var.actions_runner_github_app_private_key
+  source = "git::https://github.com/tenzin-io/terraform-tenzin-homelab.git//kubernetes/actions-runner?ref=main"
+}
+
+module "actions_runner_user" {
+  depends_on      = [module.actions_runner]
+  runner_set_name = "tlhakhan"
+  source          = "git::https://github.com/tenzin-io/terraform-tenzin-homelab.git//kubernetes/actions-runner-set?ref=main"
+  runner_image    = "ghcr.io/tenzin-io/actions-runner:0.0.2"
+  github_config_urls = [
+    "https://github.com/tlhakhan/learn-wasm",
+    "https://github.com/tlhakhan/learn-rust"
+  ]
+  github_app_id              = data.vault_generic_secret.github_tlhakhan_user.data["github_app_id"]
+  github_app_installation_id = data.vault_generic_secret.github_tlhakhan_user.data["github_app_installation_id"]
+  github_app_private_key     = data.vault_generic_secret.github_tlhakhan_user.data["github_app_private_key"]
+}
+
+module "actions_runner_org" {
+  depends_on                 = [module.actions_runner]
+  runner_set_name            = "tenzin-io"
+  source                     = "git::https://github.com/tenzin-io/terraform-tenzin-homelab.git//kubernetes/actions-runner-set?ref=main"
+  runner_image               = "ghcr.io/tenzin-io/actions-runner:0.0.2"
+  github_config_urls         = ["https://github.com/tenzin-io"]
+  github_app_id              = data.vault_generic_secret.github_tenzin_org.data["github_app_id"]
+  github_app_installation_id = data.vault_generic_secret.github_tenzin_org.data["github_app_installation_id"]
+  github_app_private_key     = data.vault_generic_secret.github_tenzin_org.data["github_app_private_key"]
 }
